7d88f59e035c31b0f990e0953a4c222efbd46f1d
sig2 <sig2@aber.ac.uk>
2015-10-27 17:42:42 +0000

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sig2 on 27/10/2015.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+/**
+ * Created by sig2 on 27/10/2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
