d0b400d34f66d43e47cff5b9240a2a60440b0558
s.griffiths <sig2@aber.ac.uk>
2015-11-23 23:53:05 +0000

    move to hibernate

--- src/main/java/com/siongriffiths/se315/configs/BeanConfigs.java
+++ src/main/java/com/siongriffiths/se315/configs/BeanConfigs.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class BeanConfigs {
-    
+
     @Bean
     public OrderService orderService(){
         return new OrderService();
