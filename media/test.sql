28c9b23298631570cfffa4b7d8393ab9cc6606fb
s.griffiths <sig2@aber.ac.uk>
2015-11-24 09:23:23 +0000

    move to hibernate

---
+++ media/test.sql
@@ -0,0 +1,6 @@
+INSERT INTO wines(bottleSize,countryOfOrigin,grapeType,longDescription,price,shortDescription,vegetarian,name) VALUES
+('big','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine1'),
+('med','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine2'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine3'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine3');
+
--- src/main/java/com/siongriffiths/se315/Application.java
+++ src/main/java/com/siongriffiths/se315/Application.java
@@ -1,10 +1,7 @@
 package com.siongriffiths.se315;
 
-import com.siongriffiths.se315.orders.OrderService;
-import com.siongriffiths.se315.wine.WineService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by sig2 on 27/10/2015.
