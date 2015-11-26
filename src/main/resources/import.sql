6347f4979c856c8ec9d22b69895f6a083d181481
s.griffiths <sig2@aber.ac.uk>
2015-11-26 12:15:39 +0000

    move to hibernate

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO wines(bottle_size,country_of_origin,grape_type,long_description,price,short_description,vegetarian,name,product_number) VALUES
+('big','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine01','0023'),
+('med','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine02','0044'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine03','0003'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine04','0004'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine05','0005'),
+('small','uk','grape1','long description of wine is long','1.29','shrt desc shrt',1,'wine06','0006'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine07','0007'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine08','0008'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine09','0009'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine10','0010'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine11','0011'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine12','0012'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine14','0013'),
+('small','uk','grape1','long description of wine is long','2.99','shrt desc shrt',1,'wine15','0014');
+
